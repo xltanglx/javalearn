69820504c5f1be4348619e411c0da904b8b4eae6
xltanglx <xltanglx@163.com>
2023-02-21 00:56:52 +0800

    工厂设计模式（简单工厂、工厂方法、抽象工厂）

---
+++ designpattern/src/main/java/designpattern/factory/absfactory/pizzastore/order/BJFactory.java
@@ -0,0 +1,24 @@
+package designpattern.factory.absfactory.pizzastore.order;
+
+import designpattern.factory.absfactory.pizzastore.pizza.BJCheesePizza;
+import designpattern.factory.absfactory.pizzastore.pizza.BJPepperPizza;
+import designpattern.factory.absfactory.pizzastore.pizza.Pizza;
+
+//这是工厂子类
+public class BJFactory implements AbsFactory {
+
+	@Override
+	public Pizza createPizza(String orderType) {
+		System.out.println("~使用的是抽象工厂模式~");
+		// TODO Auto-generated method stub
+		Pizza pizza = null;
+		if(orderType.equals("cheese")) {
+			pizza = new BJCheesePizza();
+		} else if (orderType.equals("pepper")){
+			pizza = new BJPepperPizza();
+		}
+		return pizza;
+	}
+
+}
+
---
+++ designpattern/src/main/java/designpattern/factory/factorymethod/pizzastore/pizza/BJPepperPizza.java
@@ -0,0 +1,12 @@
+package designpattern.factory.factorymethod.pizzastore.pizza;
+
+public class BJPepperPizza extends Pizza {
+
+	@Override
+	public void prepare() {
+		// TODO Auto-generated method stub
+		setName("北京的胡椒pizza");
+		System.out.println(" 北京的胡椒pizza 准备原材料");
+	}
+
+}
